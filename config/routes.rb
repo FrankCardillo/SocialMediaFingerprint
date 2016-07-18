91cb2fab1172b88054561fc065b3016ad69dfb3a
Frank Cardillo <facardillo91@gmail.com>
2016-07-18 19:16:03 -0400

    Preliminary Authentication Functionality Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+    authenticated :user do
+      root 'tests#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :tests, only: :index
 end
