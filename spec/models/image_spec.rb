672542588223a99fb8be0c04d7daeaf9817e27d7
Frank Cardillo <facardillo91@gmail.com>
2016-07-24 16:02:47 -0400

    Fixing hound violations

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,7 +3,9 @@ require 'shoulda-matchers'
 require 'valid_attribute'
 
 describe Image do
-  it { should allow_value(File.open(File.join(Rails.root, '/spec/support/test.png'))).for(:image_url) }
-  it { should_not allow_value(File.open(File.join(Rails.root, '/spec/support/bad_image.jpg'))).for(:image_url) }
+  it { should allow_value(File.open(File.join(Rails.root,
+    '/spec/support/test.png'))).for(:image_url) }
+  it { should_not allow_value(File.open(File.join(Rails.root,
+    '/spec/support/bad_image.jpg'))).for(:image_url) }
   it { should belong_to(:user) }
 end
--- spec/requests/users_sessions_request_spec.rb
+++ spec/requests/users_sessions_request_spec.rb
@@ -4,14 +4,14 @@ describe "GET '/auth/facebook/callback'" do
   before(:each) do
     valid_facebook_login_setup
     get "/users/auth/facebook/callback"
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
   end
 
-  it "sets the user_id" do
-    expect(session["warden.user.user.key"][0][0]).to eq(User.last.id)
+  it 'sets the user_id' do
+    expect(session['warden.user.user.key'][0][0]).to eq(User.last.id)
   end
 
-  it "redirects to authenticated_root" do
+  it 'redirects to authenticated_root' do
     expect(response).to redirect_to 'http://www.example.com/images'
   end
 end
