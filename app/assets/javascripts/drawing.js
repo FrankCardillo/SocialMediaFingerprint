d6be8cfdea37f7dfa9c4492b1f3f0a038304614e
Frank Cardillo <facardillo91@gmail.com>
2016-07-26 14:38:53 -0400

    Restyled index and new pages, deleted unneeded image assets, edited test to reflect new button names

--- app/assets/javascripts/drawing.js
+++ app/assets/javascripts/drawing.js
@@ -1,10 +1,13 @@
 Hydration.onReady(function(data){
-  console.log('User data:', data.userData);
+
   function drawImage() {
-    var point = new Point(data.userData["link"].length, data.userData["link"].length);
+    var point = new Point(data.userData['link'].length, data.userData['link'].length);
     var size = new Size(30);
     var path = new Path.Rectangle(point, size);
     path.strokeColor = 'red';
   };
-  drawImage();
+
+  $('#drawer').click(function() {
+    drawImage();
+  });
 });
