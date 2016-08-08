fc1063aae0b84b4c670b11e45f0dadb134e31c5c
Frank Cardillo <facardillo91@gmail.com>
2016-08-08 14:27:19 -0400

    Getting rid of useless devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, path_names: {
+  sign_up: '',
+  edit: '',
+  password: '',
+  cancel: ''
+}
 
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy'
